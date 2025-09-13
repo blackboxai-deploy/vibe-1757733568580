68b620d4b4363572f2b1b99c3b6763aedd57b929
blackboxai-deploy <deploy@blackboxai.dev>
2025-09-13 08:49:35 +0530

    Add src/app/page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import TicTacToeGame from '@/components/TicTacToeGame';
+
+export default function HomePage() {
+  return (
+    <div className="w-full min-h-screen flex items-center justify-center p-4">
+      <TicTacToeGame />
+    </div>
+  );
+}
